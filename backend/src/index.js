31828fefae75c62ebf36204044a713d304a7baf6
Lucas Gabriel <lucasggtelles@gmail.com>
2020-03-25 00:34:41 -0300

    Back-end da aplicaçao

---
+++ backend/src/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+
+const routes = require('./routes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+/**
+ * Rotas / Recursos
+ * (Todo) / (Parte) 
+ */
+
+/** 
+ * Tipos de parâmetros:
+ * 
+ * Query Params: Parâmetros nomeados enviados na rota após "?" (Filtros, Paginação)
+ * Route Params: Parâmetros utilizados para identificar recursos
+ * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
+**/ 
+
+app.listen(3333);
